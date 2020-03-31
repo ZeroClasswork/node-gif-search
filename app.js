56696fd4ba049da795e3717ae12b96d313c2670a
atleastzero <megan.obryan52@gmail.com>
2020-03-31 11:38:07 -0700

    Added working gif search

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Require Libraries
 const express = require('express');
+require('dotenv').config();
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": process.env.API_KEY, // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
 
 // Setup Express App
 const app = express();
@@ -11,10 +18,20 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
-  app.get('/', (req, res) => {
-    console.log(req.query) // => "{ term: hey" }
-
-    res.render('home')
+app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
   })
 
 app.get('/greetings/:name', (req, res) => {
@@ -22,7 +39,7 @@ app.get('/greetings/:name', (req, res) => {
     const name = req.params.name;
     // render the greetings view, passing along the name
     res.render('greetings', { name });
-  })
+})
 
 // Start Server
 app.listen(3000, () => {
